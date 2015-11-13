78847953e16449b1a1a2705635e9d3e0a849cfc7
aksenov.vitalii <aksenov.vitaly@gmail.com>
2015-11-14 01:49:29 +0300

    make updates to the login fo

--- src/main/java/ru/ifmo/acm/MyUI.java
+++ src/main/java/ru/ifmo/acm/MyUI.java
@@ -1,17 +1,19 @@
 package ru.ifmo.acm;
 
-import javax.servlet.annotation.WebServlet;
-
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.annotations.Widgetset;
+import com.vaadin.navigator.Navigator;
+import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.*;
+import com.vaadin.ui.themes.ChameleonTheme;
+import com.vaadin.ui.themes.Reindeer;
+import com.vaadin.ui.themes.ValoTheme;
+import ru.ifmo.acm.login.LoginView;
+
+import javax.servlet.annotation.WebServlet;
 
 /**
  *
@@ -22,19 +24,59 @@ public class MyUI extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
-        layout.setMargin(true);
-        setContent(layout);
+        VerticalLayout rootLayout = new VerticalLayout();
+        final MenuBar menu = new MenuBar();
+        menu.setStyleName(ValoTheme.MENUBAR_BORDERLESS);
+        menu.setVisible(false);
 
-        Button button = new Button("Click Me");
-        button.addClickListener(new Button.ClickListener() {
-            @Override
-            public void buttonClick(ClickEvent event) {
-                layout.addComponent(new Label("Thank you for clicking"));
+        final VerticalLayout content = new VerticalLayout();
+        content.setMargin(false);
+        content.setSpacing(true);
+        content.setSizeFull();
+
+        rootLayout.addComponents(menu, content);
+        rootLayout.setComponentAlignment(menu, Alignment.MIDDLE_CENTER);
+
+        setContent(rootLayout);
+
+        new Navigator(this, content);
+
+        getNavigator().addView(LoginView.NAME, LoginView.class);
+
+        getNavigator().addView(MainView.NAME, MainView.class);
+
+        menu.addItem("Logout", new MenuBar.Command() {
+            public void menuSelected(final MenuBar.MenuItem selectedItem) {
+                getSession().setAttribute("user", null);
+                getNavigator().navigateTo("");
             }
         });
-        layout.addComponent(button);
 
+        getNavigator().addViewChangeListener(new ViewChangeListener() {
+            public boolean beforeViewChange(ViewChangeEvent e) {
+                boolean isLoggedIn = getSession().getAttribute("user") != null;
+                boolean isLoginView = e.getNewView() instanceof LoginView;
+                if (isLoggedIn && isLoginView) {
+                    return false;
+                } else if (!isLoggedIn && !isLoginView) {
+                    getNavigator().navigateTo(LoginView.NAME);
+                    return false;
+                }
+                if (!isLoginView) {
+                    menu.setVisible(true);
+                } else {
+                    menu.setVisible(false);
+                }
+
+                return true;
+            }
+
+            public void afterViewChange(ViewChangeEvent e) {
+                Notification.show("Navigated to " + e.getViewName());
+            }
+        });
+
+        getNavigator().navigateTo(LoginView.NAME);
     }
 
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
