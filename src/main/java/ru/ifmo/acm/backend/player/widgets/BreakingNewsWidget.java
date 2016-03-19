a371012dc9859e4a0e8e924181a3caa25f960da8
aksenov.vitalii <aksenov.vitaly@gmail.com>
2016-03-20 00:40:15 +0300

    Breaking news widget

--- src/main/java/ru/ifmo/acm/backend/player/widgets/BreakingNewsWidget.java
+++ src/main/java/ru/ifmo/acm/backend/player/widgets/BreakingNewsWidget.java
@@ -13,12 +13,20 @@ import java.awt.*;
 public class BreakingNewsWidget extends VideoWidget {
     private int x;
     private int y;
-    private int width;
-    private int height;
     private int duration;
+    private int wVideo;
+    private int hVideo;
+    private final int PLATE_WIDTH;
+    private final int GAP;
 
     public BreakingNewsWidget(int x, int y, int width, int height, double aspectRatio, int sleepTime, int duration) {
         super(x, y, width, (int) (height / aspectRatio), sleepTime, 0);
+        wVideo = width;
+        hVideo = (int) (height / aspectRatio);
+
+        PLATE_WIDTH = (int) (1.5 * width);
+        GAP = (int) (0.05 * hVideo);
+
         this.x = x;
         this.y = y;
         this.width = width;
@@ -30,6 +38,8 @@ public class BreakingNewsWidget extends VideoWidget {
     private RunInfo run;
     private TeamInfo team;
     private boolean isLive;
+    private TeamInfo currentShow;
+    private double timer;
 
     @Override
     protected void updateImpl(Data data) {
@@ -47,7 +57,7 @@ public class BreakingNewsWidget extends VideoWidget {
             int teamId = data.breakingNewsData.teamId;
             int problemId = data.breakingNewsData.problemId;
 
-            team = Preparation.eventsLoader.getContestData().getParticipant(teamId);
+            team = Preparation.eventsLoader.getContestData().getParticipant(teamId).getSmallTeamInfo();
             java.util.List<RunInfo> runs = team.getRuns()[problemId];
             run = runs.get(runs.size() - 1);
             for (RunInfo run1 : runs) {
@@ -65,6 +75,22 @@ public class BreakingNewsWidget extends VideoWidget {
 
             change(url);
             isLive = data.breakingNewsData.isLive;
+
+            if (run.getResult().equals("AC")) {
+                if (run.getTime() == Preparation.eventsLoader.getContestData().firstTimeSolved()[problemId]) {
+                    caption = "First to solve";
+                } else if (team.getRank() <= 12) {
+                    caption = "Get to " + team.getRank() + " by solving";
+                } else {
+                    caption = "Solved";
+                }
+            } else if (run.getResult().length() == 0) {
+                caption = "Submitted";
+            }
+            caption += " problem " + (char) ('A' + problemId);
+
+            currentShow = run.getTeamInfoBefore();
+            timer = 0;
         }
 
         lastUpdate = System.currentTimeMillis();
@@ -74,8 +100,27 @@ public class BreakingNewsWidget extends VideoWidget {
     public void paintImpl(Graphics2D g, int width, int height) {
         update();
 
-        if (!isVisible()) {
+        if (visibilityState == 0) {
+            stop();
             return;
         }
+        int dt = updateVisibilityState();
+
+        timer += dt;
+
+        if (timer > duration / 2) {
+            currentShow = team;
+        }
+
+        if (run == null || URL.get() == null) {
+            int hh = (int) (hVideo * opacity);
+            g.drawImage(image.get(), x, y + (hVideo - hh) / 2, wVideo, hh, null);
+        }
+
+        int y = this.y + hVideo + GAP;
+        int x = this.x + (wVideo - PLATE_WIDTH) / 2;
+        drawTeamPane(g, currentShow, x, y + hVideo, PLATE_WIDTH, visibilityState);
+        drawTextInRect(g, caption, (int) (x - 0.005 * PLATE_WIDTH), y, -1, PLATE_WIDTH / 10,
+                POSITION_RIGHT, ACCENT_COLOR, Color.white, visibilityState);
     }
 }
--- src/main/java/ru/ifmo/acm/events/SmallTeamInfo.java
+++ src/main/java/ru/ifmo/acm/events/SmallTeamInfo.java
@@ -1,26 +1,50 @@
 package ru.ifmo.acm.events;
 
+import java.util.List;
+
 /**
  * Created by Aksenov239 on 19.03.2016.
  */
-public class SmallTeamInfo {
+public class SmallTeamInfo implements TeamInfo {
     private int rank, solved, penalty;
+    private String shortName;
 
-    public SmallTeamInfo(int rank, int solved, int penalty) {
-        this.rank = rank;
-        this.solved = solved;
-        this.penalty = penalty;
+    public SmallTeamInfo(TeamInfo team) {
+        rank = team.getRank();
+        solved = team.getSolvedProblemsNumber();
+        penalty = team.getPenalty();
+        shortName = team.getShortName();
+    }
+
+    public int getId() {
+        return -1;
     }
 
     public int getRank() {
         return rank;
     }
 
-    public int getSolve() {
+    public String getName() {
+        return null;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public int getSolvedProblemsNumber() {
         return solved;
     }
 
     public int getPenalty() {
         return penalty;
+    }
+
+    public long getLastAccepted() {
+        return 0;
+    }
+
+    public List<RunInfo>[] getRuns() {
+        return null;
     }
 }
