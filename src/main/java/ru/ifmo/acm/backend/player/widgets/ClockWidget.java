7339100e48b6be63aea309d687dbb95edece3054
aksenov.vitalii <aksenov.vitaly@gmail.com>
2016-03-19 15:33:08 +0300

    countdown time fixed

--- src/main/java/ru/ifmo/acm/backend/player/widgets/ClockWidget.java
+++ src/main/java/ru/ifmo/acm/backend/player/widgets/ClockWidget.java
@@ -54,7 +54,7 @@ public class ClockWidget extends Widget {
         drawRect(g, x, y, WIDTH, HEIGHT, DARK_GRAY, opacity);
         g.setColor(Color.WHITE);
         g.setFont(clockFont);
-        long time = Preparation.eventsLoader.getContestData().getCurrentTime() / 1000;
+        long time = Math.abs(Preparation.eventsLoader.getContestData().getCurrentTime() / 1000);
         //System.err.println("Clock time: " + time);
         if (time > 5 * 60 * 60) {
             time = 5 * 60 * 60;
