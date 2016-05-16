0e80e6f9eafdcbd6f087e94de394be66591a129d
Egor Kulikov <egor@egork.net>
2016-05-16 13:26:13 +0700

    Analyst message backend

---
+++ src/main/java/ru/ifmo/acm/events/WF/WFAnalystMessage.java
@@ -0,0 +1,128 @@
+package ru.ifmo.acm.events.WF;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * @author egor@egork.net
+ */
+public class WFAnalystMessage implements ru.ifmo.acm.events.AnalystMessage {
+    private static Logger log = LogManager.getLogger(WFAnalystMessage.class);
+
+    private int id = -1;
+    private int team = -1;
+    private int time = -1;
+    private int priority = -1;
+    private int problem = -1;
+    private int runId = -1;
+    private WFAnalystMessageCategory category;
+    private String message;
+
+    public enum WFAnalystMessageCategory {
+        AUTO("auto"),
+        HUMAN("human");
+
+        public final String key;
+
+
+        WFAnalystMessageCategory(String key) {
+            this.key = key;
+        }
+
+        public static WFAnalystMessageCategory getCategory(String category) {
+            for (WFAnalystMessageCategory aCategory: values()) {
+                if (category.equals(aCategory.key)) {
+                    return aCategory;
+                }
+            }
+            log.warn("Unknown category: " + category);
+            return HUMAN;
+        }
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public int getTeam() {
+        return team;
+    }
+
+    public void setTeam(int team) {
+        this.team = team;
+    }
+
+    @Override
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    @Override
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    @Override
+    public int getProblem() {
+        return problem;
+    }
+
+    public void setProblem(int problem) {
+        this.problem = problem;
+    }
+
+    @Override
+    public int getRunId() {
+        return runId;
+    }
+
+    public void setRunId(int runId) {
+        this.runId = runId;
+    }
+
+    @Override
+    public WFAnalystMessageCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(WFAnalystMessageCategory category) {
+        this.category = category;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "WFAnalystMessage{" +
+                "id=" + id +
+                ", team=" + team +
+                ", time=" + time +
+                ", priority=" + priority +
+                ", problem=" + problem +
+                ", runId=" + runId +
+                ", category=" + category +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
