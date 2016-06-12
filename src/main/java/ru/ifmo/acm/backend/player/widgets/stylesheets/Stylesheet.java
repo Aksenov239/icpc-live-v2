eabd38b2df89b79f528c21433799bff35292ce9d
aksenov.vitalii <aksenov.vitaly@gmail.com>
2016-06-12 22:26:45 +0300

    first change with stylesheets

---
+++ src/main/java/ru/ifmo/acm/backend/player/widgets/stylesheets/Stylesheet.java
@@ -0,0 +1,40 @@
+package ru.ifmo.acm.backend.player.widgets.stylesheets;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Properties;
+
+/**
+ * Created by Aksenov239 on 12.06.2016.
+ */
+public class Stylesheet {
+    public static HashMap<String, String> colors = new HashMap<>();
+
+    static {
+        Properties mainProperties = new Properties();
+        Properties properties = new Properties();
+        try {
+            mainProperties.load(Stylesheet.class.getResourceAsStream("mainscreen.properties"));
+            String stylesheet = mainProperties.getProperty("stylesheet");
+
+            properties.load(Stylesheet.class.getResourceAsStream(stylesheet));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (String name : properties.stringPropertyNames()) {
+            colors.put(name, properties.getProperty(name));
+        }
+
+        boolean changed = true;
+        while (changed) {
+            changed = false;
+            for (String name : properties.stringPropertyNames()) {
+                if (colors.containsKey(colors.get(name))) {
+                    changed = true;
+                    colors.put(name, colors.get(colors.get(name)));
+                }
+            }
+        }
+    }
+}
