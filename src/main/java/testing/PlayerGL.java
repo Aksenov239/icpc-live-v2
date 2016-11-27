f1c1ce89da0c299300d49e7451ebf386097e82ee
aksenov.vitalii <aksenov.vitaly@gmail.com>
2016-11-27 21:07:21 +0300

    The player works but badly

--- src/main/java/testing/PlayerGL.java
+++ src/main/java/testing/PlayerGL.java
@@ -1,22 +1,26 @@
 package testing;
 
 import com.jogamp.common.net.Uri;
-import com.jogamp.opengl.GL;
-import com.jogamp.opengl.GL2;
-import com.jogamp.opengl.GLAutoDrawable;
-import com.jogamp.opengl.GLEventListener;
+import com.jogamp.common.util.InterruptSource;
+import com.jogamp.newt.opengl.GLWindow;
+import com.jogamp.opengl.*;
 import com.jogamp.opengl.awt.GLCanvas;
 import com.jogamp.opengl.util.FPSAnimator;
 import com.jogamp.opengl.util.av.GLMediaPlayer;
 import com.jogamp.opengl.util.av.GLMediaPlayerFactory;
 import com.jogamp.opengl.util.texture.Texture;
 import com.jogamp.opengl.util.texture.TextureCoords;
+import com.jogamp.opengl.util.texture.TextureData;
+import com.jogamp.opengl.util.texture.TextureSequence;
+import testing.jogl.MovieSimple;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.net.URISyntaxException;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 /**
  * Created by Aksenov239 on 23.11.2016.
@@ -25,7 +29,7 @@ public class PlayerGL implements GLEventListener {
 
     /* If FFMPEG is in libav folder */
 //    static {
-//        String directory = ".../libav/x64/";
+//        String directory = "C:/work/svn/icpc-live-v2/libav/x64/";
 //
 //        System.load(directory + "avutil-55.dll");
 //        System.load(directory + "postproc-54.dll");
@@ -37,19 +41,145 @@ public class PlayerGL implements GLEventListener {
 //        System.load(directory + "avdevice-57.dll");
 //    }
 
-    GLMediaPlayer player;
 
-    @Override
-    public void init(GLAutoDrawable drawable) {
-        GL2 gl = drawable.getGL().getGL2();
+    static class MyGLMediaEventListener implements GLMediaPlayer.GLMediaEventListener {
+        void destroyWindow(final com.jogamp.newt.Window window) {
+            new InterruptSource.Thread() {
+                public void run() {
+                    window.destroy();
+                }
+            }.start();
+        }
 
+        @Override
+        public void newFrameAvailable(final GLMediaPlayer ts, final TextureSequence.TextureFrame newFrame, final long when) {
+        }
+
+        @Override
+        public void attributesChanged(final GLMediaPlayer mp, final int event_mask, final long when) {
+            System.err.println("MovieSimple AttributesChanges: events_mask 0x" + Integer.toHexString(event_mask) + ", when " + when);
+            System.err.println("MovieSimple State: " + mp);
+            final GLWindow window = (GLWindow) mp.getAttachedObject(MovieSimple.WINDOW_KEY);
+            final PlayerGL ms = (PlayerGL) mp.getAttachedObject(MovieSimple.PLAYER);
+            if (0 != (GLMediaPlayer.GLMediaEventListener.EVENT_CHANGE_SIZE & event_mask)) {
+                System.err.println("MovieSimple State: CHANGE_SIZE");
+            }
+            if (0 != (GLMediaPlayer.GLMediaEventListener.EVENT_CHANGE_INIT & event_mask)) {
+                System.err.println("MovieSimple State: INIT");
+                // Use GLEventListener in all cases [A+V, V, A]
+                window.addGLEventListener(ms);
+                final GLAnimatorControl anim = window.getAnimator();
+                anim.setUpdateFPSFrames(60, null);
+                anim.resetFPSCounter();
+            }
+            if (0 != (GLMediaPlayer.GLMediaEventListener.EVENT_CHANGE_PLAY & event_mask)) {
+                window.getAnimator().resetFPSCounter();
+            }
+
+            boolean destroy = false;
+            Throwable err = null;
+
+            if (0 != (GLMediaPlayer.GLMediaEventListener.EVENT_CHANGE_EOS & event_mask)) {
+                err = ms.player.getStreamException();
+                if (null != err) {
+                    System.err.println("MovieSimple State: EOS + Exception");
+                    destroy = true;
+                } else {
+                    System.err.println("MovieSimple State: EOS");
+                    destroy = true;
+                }
+            }
+            if (0 != (GLMediaPlayer.GLMediaEventListener.EVENT_CHANGE_ERR & event_mask)) {
+                err = ms.player.getStreamException();
+                if (null != err) {
+                    System.err.println("MovieSimple State: ERR + Exception");
+                } else {
+                    System.err.println("MovieSimple State: ERR");
+                }
+                destroy = true;
+            }
+            if (destroy) {
+                if (null != err) {
+                    err.printStackTrace();
+                }
+                destroyWindow(window);
+            }
+        }
+    }
+
+    public final static MyGLMediaEventListener myGLMediaEventListener = new MyGLMediaEventListener();
+
+    public GLMediaPlayer player;
+    public Texture textureToShow;
+
+    public void translateToProperFormat(GL2GL3 gl, Texture texture) {
+        int width = texture.getWidth();
+        int height = texture.getHeight();
+        ByteBuffer buffer = ByteBuffer.allocate(3 * width * height);
+        // To store previous picture, done for YUV, that is why 3
+        gl.glGetTexImage(GL.GL_TEXTURE_2D, 0, GL.GL_RGB, GL.GL_UNSIGNED_BYTE, buffer);
+        byte[] original = buffer.array();
+
+        int resWidth = player.getWidth();
+        int resHeight = player.getHeight();
+        byte[] convertedBuffer = new byte[3 * resWidth * resHeight];
+        // YUV420p
+        int p = 0;
+        for (int y = 0; y < resHeight; y++) {
+            for (int x = 0; x < resWidth; x++) {
+                int yc = y * width + x;
+                int uc = (y / 2) * width + (resWidth + x / 2);
+                int vc = (y / 2 + height / 2) * width + (resWidth + x / 2);
+
+                int yy = original[3 * yc];
+                int u = original[3 * uc];
+                int v = original[3 * vc];
+
+                yy = yy < 0 ? yy + 256 : yy; // get only r
+                u = u < 0 ? u + 256 : u;
+                v = v < 0 ? v + 256 : v;
+
+                if (yy < 0 || yy > 255 || u < 0 || u > 255 || v < 0 || v > 255) {
+                    throw new AssertionError();
+                }
+
+                double r = 1.1643 * (yy - 16) + 1.5958 * (v - 128);
+                double g = 1.1643 * (yy - 16) - 0.8129 * (v - 128) - 0.39173 * (u - 128);
+                double b = 1.1643 * (yy - 16) + 2.017 * (u - 128);
+                convertedBuffer[p++] = (byte) r;
+                convertedBuffer[p++] = (byte) g;
+                convertedBuffer[p++] = (byte) b;
+            }
+        }
+
+        TextureData data = new TextureData(gl.getGLProfile(), GL.GL_RGB, resWidth, resHeight, 0,
+                GL.GL_RGB, GL.GL_UNSIGNED_BYTE
+                , false, false, true, ByteBuffer.wrap(convertedBuffer), null);
+        if (textureToShow == null) {
+            textureToShow = new Texture(gl, data);
+        } else {
+            textureToShow.updateImage(gl, data);
+        }
+    }
+
+    public PlayerGL() {
         player = GLMediaPlayerFactory.create(GLMediaPlayer.class.getClassLoader(), "jogamp.opengl.util.av.impl.FFMPEGMediaPlayer");
         try {
-            player.initStream(Uri.cast("pics/BigBuckBunny_320x180.mp4"),
+            player.initStream(
+//                    Uri.cast("http://archive.org/download/BigBuckBunny_328/BigBuckBunny_512kb.mp4"),
+                    Uri.cast("pics/BigBuckBunny_320x180.mp4"),
                     GLMediaPlayer.STREAM_ID_AUTO, GLMediaPlayer.STREAM_ID_AUTO, 4);
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }
+        player.attachObject(MovieSimple.PLAYER, this);
+    }
+
+    @Override
+    public void init(GLAutoDrawable drawable) {
+        GL2ES2 gl = drawable.getGL().getGL2ES2();
+
+        player.setTextureMinMagFilter(new int[]{GL.GL_NEAREST, GL.GL_LINEAR});
 
         while (player.getState() != GLMediaPlayer.State.Initialized) {
         }
@@ -63,6 +193,8 @@ public class PlayerGL implements GLEventListener {
         player.play();
         while (player.getState() != GLMediaPlayer.State.Playing) {
         }
+
+        gl.glEnable(GL.GL_DEPTH_TEST);
     }
 
     @Override
@@ -74,12 +206,19 @@ public class PlayerGL implements GLEventListener {
     public void display(GLAutoDrawable drawable) {
         GL2 gl = drawable.getGL().getGL2();
 
-        gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_TEST);
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
 
         Texture texture = player.getNextTexture(gl).getTexture();
 
         texture.enable(gl);
         texture.bind(gl);
+
+        translateToProperFormat(gl, texture);
+
+        texture.disable(gl);
+
+        textureToShow.enable(gl);
+        textureToShow.bind(gl);
 
         TextureCoords coords = texture.getImageTexCoords();
 
@@ -94,7 +233,7 @@ public class PlayerGL implements GLEventListener {
         gl.glVertex2d(0, 1);
         gl.glEnd();
 
-        texture.disable(gl);
+        textureToShow.disable(gl);
     }
 
     @Override
@@ -112,9 +251,10 @@ public class PlayerGL implements GLEventListener {
 
     public static void main(String[] args) {
         GLCanvas canvas = new GLCanvas();
+
         canvas.addGLEventListener(new PlayerGL());
-        canvas.setPreferredSize(new Dimension(1280, 720));
-        final FPSAnimator animator = new FPSAnimator(canvas, 10, true);
+        canvas.setPreferredSize(new Dimension(320, 180));
+        final FPSAnimator animator = new FPSAnimator(canvas, 30, true);
         final JFrame frame = new JFrame();
         frame.getContentPane().add(canvas);
         frame.addWindowListener(new WindowAdapter() {
