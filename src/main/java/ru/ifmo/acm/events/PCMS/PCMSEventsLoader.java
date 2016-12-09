7ce6e695e7c7262edb803a66e0b582e8e2482acf
Pavel Mavrin <pavel.mavrin@gmail.com>
2016-12-09 22:37:06 +0300

    fix for vkoshp

--- src/main/java/ru/ifmo/acm/events/PCMS/PCMSEventsLoader.java
+++ src/main/java/ru/ifmo/acm/events/PCMS/PCMSEventsLoader.java
@@ -71,6 +71,8 @@ public class PCMSEventsLoader extends EventsLoader {
             if (shortName == null || shortName.length() == 0) {
               int index = participantName.indexOf("(");
               shortName = participantName.substring(0, index - 1);
+              int index = shortName.indexOf(",");
+              shortName = shortName.substring(index + 2);
             }
             String region = participant.attr("region");
             String hashTag = participant.attr("hashtag");
