bc66ae68379d1fff2d69a6e910044b8fcd1c5270
Pavel Mavrin <pavel.mavrin@gmail.com>
2019-03-31 20:32:29 +0300

    small fixes

--- src/main/java/org/icpclive/backend/player/widgets/LocatorWidget.java
+++ src/main/java/org/icpclive/backend/player/widgets/LocatorWidget.java
@@ -15,10 +15,13 @@ import java.util.List;
 
 public class LocatorWidget extends Widget {
 
+    public static final int TOP_Y = 50;
+    private static final int BOTTOM_Y = 968;
+    private static final int QUEUE_WIDTH = 550;
     private LocatorData data;
     private static final int TEAM_PANE_HEIGHT = 41;
     private static final int TEAM_PANE_WIDTH = getTeamPaneWidth(TEAM_PANE_HEIGHT);
-    private static final int MARGIN = 50;
+    private static final int MARGIN = 30;
 
     public LocatorWidget(long updateWait) {
         super(updateWait);
@@ -60,39 +63,93 @@ public class LocatorWidget extends Widget {
         g2.setColor(new Color(0, 0, 0, (int) (textOpacity * 120)));
         g2.fill(area);
 
-        int[] x = placeTeamPanes(c, width);
+        int[] x = new int[c.length];
+        int pos = 0;
+        int minPenalty = Integer.MAX_VALUE;
+
+        for (int m = 0; m < (1 << c.length); m++) {
+            int n1 = Integer.bitCount(m);
+            int n2 = c.length - n1;
+
+            if (n1 > 3 || n2 > 2) continue;
+
+            int[][] cTop = new int[n1][];
+            int[][] cBottom = new int[n2][];
+            int k1 = 0;
+            int k2 = 0;
+            for (int i = 0; i < c.length; i++) {
+                if (((m >> i) & 1) == 1) {
+                    cTop[k1++] = c[i];
+                } else {
+                    cBottom[k2++] = c[i];
+                }
+            }
+            int s = 0;
+            int[] xTop = placeTeamPanes(
+                    cTop,
+                    TEAM_PANE_WIDTH / 2 + MARGIN,
+                    width - TEAM_PANE_WIDTH / 2 - MARGIN,
+                    TOP_Y);
+            s += penalty;
+            int[] xBottom = placeTeamPanes(
+                    cBottom,
+                    TEAM_PANE_WIDTH / 2 + MARGIN + QUEUE_WIDTH,
+                    width - TEAM_PANE_WIDTH / 2 - MARGIN,
+                    BOTTOM_Y);
+            s += penalty;
+
+            if (s < minPenalty) {
+                minPenalty = s;
+                pos = m;
+                k1 = k2 = 0;
+                for (int i = 0; i < c.length; i++) {
+                    if (((m >> i) & 1) == 1) {
+                        x[i] = xTop[k1++];
+                    } else {
+                        x[i] = xBottom[k2++];
+                    }
+                }
+            }
+        }
+
         for (int i = 0; i < teams.size(); i++) {
+            int side = (pos >> i) & 1;
+
             drawTeamPane(graphics, teams.get(i),
                     x[i] - TEAM_PANE_WIDTH / 2,
-                    100,
+                    side == 1 ? TOP_Y : BOTTOM_Y,
                     TEAM_PANE_HEIGHT,
                     visibilityState);
 
             int x1 = x[i];
-            int y1 = 100 + TEAM_PANE_HEIGHT + 5;
+            int y1 = side == 1 ? TOP_Y + TEAM_PANE_HEIGHT + 5 : BOTTOM_Y - 5;
             int x2 = c[i][0];
             int y2 = c[i][1];
             int r = c[i][2];
             double d = Math.hypot(x1 - x2, y1 - y2);
 
-            double d1 = (d - r) * (0.1 - textOpacity * 0.1) / d;
-            double d2 = (d - r) * (0.1 + textOpacity * 0.9) / d;
+            if (d > r * 1.1) {
+                double d1 = (d - r) * (0.1 - textOpacity * 0.1) / d;
+                double d2 = (d - r) * (0.1 + textOpacity * 0.9) / d;
 
-            int xx1 = (int) (x1 + (x2 - x1) * d1);
-            int yy1 = (int) (y1 + (y2 - y1) * d1);
-            int xx2 = (int) (x1 + (x2 - x1) * d2);
-            int yy2 = (int) (y1 + (y2 - y1) * d2);
+                int xx1 = (int) (x1 + (x2 - x1) * d1);
+                int yy1 = (int) (y1 + (y2 - y1) * d1);
+                int xx2 = (int) (x1 + (x2 - x1) * d2);
+                int yy2 = (int) (y1 + (y2 - y1) * d2);
 
-            g2.setColor(Color.WHITE);
-            g2.setStroke(new BasicStroke(3));
-            g2.drawLine(xx1, yy1, xx2, yy2);
+                g2.setColor(Color.WHITE);
+                g2.setStroke(new BasicStroke(3));
+                g2.drawLine(xx1, yy1, xx2, yy2);
+            }
 //
 //            int d = c[i][1] - 100 - TEAM_PANE_HEIGHT - c[i][2];
 //            g2.fillRect(c[i][0] - 1, 100 + TEAM_PANE_HEIGHT + (int) (d * (1 - textOpacity) / 10), 3, (int) (d * textOpacity));
         }
     }
 
-    private int[] placeTeamPanes(int[][] c, int width) {
+    int penalty;
+
+    private int[] placeTeamPanes(int[][] c, int min, int max, int y) {
         int n = c.length;
         int[][] ap = new int[n][2];
         for (int i = 0; i < n; i++) {
@@ -102,8 +159,6 @@ public class LocatorWidget extends Widget {
         Arrays.sort(ap, (o1, o2) -> Integer.compare(o1[0], o2[0]));
         int[] a = new int[n];
         for (int i = 0; i < n; i++) a[i] = ap[i][0];
-        int min = TEAM_PANE_WIDTH / 2 + MARGIN;
-        int max = width - TEAM_PANE_WIDTH / 2 - MARGIN;
         int[] b = new int[n];
         for (int i = 0; i < n; i++) {
             b[i] = min + (TEAM_PANE_WIDTH + MARGIN) * i;
@@ -111,7 +166,7 @@ public class LocatorWidget extends Widget {
         for (int i = n - 1; i >= 0; i--) {
             while (true) {
                 int[] bb = b.clone();
-                bb[i]++;
+                bb[i] += 10;
                 for (int j = i + 1; j < n; j++) {
                     bb[j] = Math.max(bb[j], bb[j - 1] + TEAM_PANE_WIDTH + MARGIN);
                 }
@@ -121,6 +176,10 @@ public class LocatorWidget extends Widget {
                     break;
                 }
             }
+        }
+        penalty = calc(a, b);
+        for (int i = 0; i < n; i++) {
+            penalty += (c[i][1] - y) * (c[i][1] - y);
         }
         int[] res = new int[n];
         for (int i = 0; i < n; i++) {
