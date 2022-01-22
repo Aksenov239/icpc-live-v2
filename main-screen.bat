99897e22adfd22f4b5f2570e4f73f9519901fabf
Lidia Perovskaya <lperovskaya@gmail.com>
2022-01-22 15:11:06 +0300

    Update main-screen.bat

--- main-screen.bat
+++ main-screen.bat
@@ -1,1 +1,1 @@
-mvn package exec:java -Dsun.java2d.opengl=True -D_sun.java2d.trace=count,out:opengl.log -Dexec.mainClass="org.icpclive.backend.Main"
+start mvn package exec:java -Dsun.java2d.opengl=True -D_sun.java2d.trace=count,out:opengl.log -Dexec.mainClass="org.icpclive.backend.Main"
